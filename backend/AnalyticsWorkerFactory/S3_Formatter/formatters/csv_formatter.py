7d2b83f1fafa8fc3fbfd8675ce3c36136c22b804
Caserefro <130204142+Caserefro@users.noreply.github.com>
2026-07-24 09:10:28 -0600

    reorganized

--- backend/AnalyticsWorkerFactory/S3_Formatter/formatters/csv_formatter.py
+++ backend/AnalyticsWorkerFactory/S3_Formatter/formatters/csv_formatter.py
@@ -37,21 +37,14 @@ class CsvFormatter:
         series_list = burndown_data if isinstance(burndown_data, list) else burndown_data.get("series", [])
         avg_est = computed_kpis.get("average_time_by_estimate", {})
 
-        # Determine estimate keys (default to E2, E4, E8, E16 if none present)
-        est_keys = list(avg_est.keys()) if avg_est else ["E2", "E4", "E8", "E16"]
+        est_keys = ["E2", "E4", "E8", "E16"]
         
-        est_headers = []
-        for k in est_keys:
-            est_headers.extend([
-                f"avg_time_todo_{k}",
-                f"avg_time_in_progress_{k}",
-                f"avg_time_in_review_{k}",
-                f"avg_time_merged_{k}"
-            ])
-
         left_headers = [
-            "record_date", "Sprint", "issues_total", "issues_todo", "issues_in_progress", "issues_in_review", "issues_merged"
-        ] + est_headers + [
+            "record_date", "Sprint", "issues_total", "issues_todo", "issues_in_progress", "issues_in_review", "issues_merged",
+            "avg_time_todo_E2", "avg_time_in_progress_E2", "avg_time_in_review_E2", "avg_time_merged_E2",
+            "avg_time_todo_E4", "avg_time_in_progress_E4", "avg_time_in_review_E4", "avg_time_merged_E4",
+            "avg_time_todo_E8", "avg_time_in_progress_E8", "avg_time_in_review_E8", "avg_time_merged_E8",
+            "avg_time_todo_E16", "avg_time_in_progress_E16", "avg_time_in_review_E16", "avg_time_merged_E16",
             "story_points_total", "story_points_bug", "story_points_non_bug", "story_points_clean_pct",
             "average_prs_per_issue", "reentries_per_issue",
             "BurndownSP", "BurndownAVGSP", "BurndownPredictionSP", "SPDelta"
@@ -75,17 +68,17 @@ class CsvFormatter:
             b_pred = pt.get("prediction_points", 0.0)
             sp_delta = round(b_rem - b_idl, 2)
             
-            est_vals = []
-            for k in est_keys:
-                e_dict = avg_est.get(k, {})
-                est_vals.extend([
-                    e_dict.get("todo", 0.0),
-                    e_dict.get("in_progress", 0.0),
-                    e_dict.get("in_review", 0.0),
-                    e_dict.get("merged", 0.0)
-                ])
+            e2 = avg_est.get("E2", {})
+            e4 = avg_est.get("E4", {})
+            e8 = avg_est.get("E8", {})
+            e16 = avg_est.get("E16", {})
             
-            row = [date, sprint_name, issues_total, issues_todo, issues_in_progress, issues_in_review, issues_merged] + est_vals + [
+            row = [
+                date, sprint_name, issues_total, issues_todo, issues_in_progress, issues_in_review, issues_merged,
+                e2.get("todo", 0.0), e2.get("in_progress", 0.0), e2.get("in_review", 0.0), e2.get("merged", 0.0),
+                e4.get("todo", 0.0), e4.get("in_progress", 0.0), e4.get("in_review", 0.0), e4.get("merged", 0.0),
+                e8.get("todo", 0.0), e8.get("in_progress", 0.0), e8.get("in_review", 0.0), e8.get("merged", 0.0),
+                e16.get("todo", 0.0), e16.get("in_progress", 0.0), e16.get("in_review", 0.0), e16.get("merged", 0.0),
                 sp_total, sp_bug, sp_non_bug, sp_clean_pct,
                 computed_kpis.get("average_prs_per_issue", 0.0), reentries,
                 b_rem, b_idl, b_pred, sp_delta
